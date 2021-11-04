7410ac169494e391b5c5ac00c2211d9798698b8d
DanteScot <kevincorasaniti@gmail.com>
2021-11-04 16:34:22 +0100

    primi gemelli

---
+++ N13.py
@@ -0,0 +1,26 @@
+a=int(input())
+b=int(input())
+aMax=round(a/2)
+bMax=round(b/2)
+primi=True
+i=2
+
+while i<=aMax:
+    if a%i==0:
+        primi=False
+    i+=1
+
+if primi:
+    i=2
+    while i<=bMax:
+        if b%i==0:
+            primi=False
+        i+=1
+
+if primi!=True:
+    print("non entrambi primi",end="")
+else:
+    if abs(a-b)==2:
+        print("gemelli",end="")
+    else:
+        print("non gemelli",end="")
